53b2589caa44396bcea6a13ff458b2b2d3fccb43
DevendraBhide <161149325+DevendraBhide@users.noreply.github.com>
2025-03-29 10:36:21 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
 
 
 
-<nav className="navbar navbar-expand-lg navbar-light bg-white px-5 py-4 ">
+<nav className="navbar navbar-expand-lg navbar-light bg-white pe-5 py-4 ">
   <div className="container-fluid">
     <NavLink className="navbar-brand" to='/'><img src="logo.jpg" alt="" /></NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
